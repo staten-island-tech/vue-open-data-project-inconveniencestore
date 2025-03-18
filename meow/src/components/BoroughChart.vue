4b22ea414d9e6e4ece93ff99e6752fa0293152c5
inconveniencestore <iivyleaves@gmail.com>
2025-03-18 12:04:30 -0400

    i had a sudden moment of clarity

--- meow/src/components/BoroughChart.vue
+++ meow/src/components/BoroughChart.vue
@@ -14,7 +14,18 @@ let chartInstance = null // store the chart instance
 
 const props = defineProps([`dataset`])
 //fix this later
-const dataset = props.dataset
+const dataset = props.dataset //shouldn't it need this?? why did dataset.map work??
+
+function sortBoroughs(borough) {
+  let counter = 0
+  dataset.forEach((element) => {
+    if (element.borough === borough) {
+      counter += 1
+    }
+  })
+
+  return counter
+}
 
 function createChart() {
   if (!chartCanvas.value) return
@@ -22,11 +33,17 @@ function createChart() {
   chartInstance = new Chart(chartCanvas.value, {
     type: 'bar',
     data: {
-      labels: dataset.map((row) => row.borough),
+      labels: ['Brooklyn', 'Manhattan', 'Queens', 'Staten Island', 'Bronx'], //dataset.map((row) => row.borough),
       datasets: [
         {
           label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
+          data: [
+            sortBoroughs('Brooklyn'),
+            sortBoroughs('Manhattan'),
+            sortBoroughs('Queens'),
+            sortBoroughs('Staten Island'),
+            sortBoroughs('Bronx'),
+          ],
           borderWidth: 1, //literally the border
         },
       ],
